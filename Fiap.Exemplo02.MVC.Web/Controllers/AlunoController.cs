4e3070e3a93b9f753e031f5faed48ea743f564ab
Nathalia Irokawa Fernandes <nathalia.iro.fernandes@gmail.com>
2016-11-04 15:47:13 -0200

    Editar e Listar Adicionados

---
+++ Fiap.Exemplo02.MVC.Web/Controllers/AlunoController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Fiap.Exemplo02.MVC.Web.Models;
+
+namespace Fiap.Exemplo02.MVC.Web.Controllers
+{
+    public class AlunoController : Controller
+    {
+        private PortalContext _context = new PortalContext();
+        // GET: Aluno
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Aluno aluno)
+        {
+            _context.Aluno.Add(aluno);
+            _context.SaveChanges();
+           
+            TempData["msg"] = "Aluno cadastrado com sucesso"; 
+            return RedirectToAction("Cadastrar");
+
+            //excluir e editar
+        }
+
+        [HttpGet]
+        public ActionResult Listar()
+        {
+            var lista = _context.Aluno.ToList();
+            return View(lista);
+        }
+
+        [HttpGet]
+        public ActionResult Editar(int id)
+        {
+            // Buscar o objeto (aluno) no banco 
+            var aluno = _context.Aluno.Find(id);
+
+            //manda o aluno para a view
+            return View(aluno);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(Aluno aluno)
+        {
+            _context.Entry(aluno).State = System.Data.Entity.EntityState.Modified;
+            _context.SaveChanges();
+            TempData["msg"] = "Aluno atualizado";
+
+            return RedirectToAction("Listar");
+        }
+    }
+}
