44211b77c890e050c16f862658cac4a4a77597ab
Nathalia Irokawa Fernandes <nathalia.iro.fernandes@gmail.com>
2016-11-04 18:23:59 -0200

    Cadastro e Listagem de Grupo e Projeto

---
+++ Fiap.Exemplo02.MVC.Web/Controllers/GrupoController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Fiap.Exemplo02.MVC.Web.Models;
+
+namespace Fiap.Exemplo02.MVC.Web.Controllers
+{
+    public class GrupoController : Controller
+    {
+        private PortalContext _context = new PortalContext();
+        // GET: Grupo
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Grupo grupo)
+        {
+            _context.Grupo.Add(grupo);
+            _context.SaveChanges();
+
+            TempData["msg"] = "Grupo cadastrado com sucesso";
+            return RedirectToAction("Cadastrar");
+        }
+
+        [HttpGet]
+        public ActionResult Listar()
+        {
+            var lista = _context.Grupo.ToList();
+            return View(lista);
+        }
+    }
+}
