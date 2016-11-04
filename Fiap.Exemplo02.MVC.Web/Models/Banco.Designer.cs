3238156a77dafc63caa6499f46f6996d52388e1b
Nathalia Irokawa Fernandes <nathalia.iro.fernandes@gmail.com>
2016-11-04 17:42:14 -0200

    Adicionado Table Projeto e Grupo no BD

--- Fiap.Exemplo02.MVC.Web/Models/Banco.Designer.cs
+++ Fiap.Exemplo02.MVC.Web/Models/Banco.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\rm12295\Desktop\Fiap.Exemplo02.MVC.Web\Fiap.Exemplo02.MVC.Web\Models\Banco.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\alunoshift\Desktop\Fiap.Exemplo02.MVC.Web\Fiap.Exemplo02.MVC.Web\Models\Banco.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
