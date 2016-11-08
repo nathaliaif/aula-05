a7f0238c42cee171cbbba0fbac9f501bbf626315
Nathalia Irokawa Fernandes <nathalia.iro.fernandes@gmail.com>
2016-11-08 14:56:41 -0200

    Adicionando Foreign Key na Tabela Aluno

--- Fiap.Exemplo02.MVC.Web/Models/Banco.Designer.cs
+++ Fiap.Exemplo02.MVC.Web/Models/Banco.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\alunoshift\Desktop\Fiap.Exemplo02.MVC.Web\Fiap.Exemplo02.MVC.Web\Models\Banco.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\alunoshift\Desktop\aula-05-master\Fiap.Exemplo02.MVC.Web\Models\Banco.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
