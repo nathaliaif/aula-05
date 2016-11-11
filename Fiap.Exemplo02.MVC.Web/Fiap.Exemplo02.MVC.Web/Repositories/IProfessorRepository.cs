1da3e863aed934afc96446716338ce749db9d36b
Nathalia Irokawa Fernandes <nathalia.iro.fernandes@hotmail.com>
2016-11-11 17:53:03 -0200

    Adicionado IProfessorRepository e ProfessorRepository

---
+++ Fiap.Exemplo02.MVC.Web/Fiap.Exemplo02.MVC.Web/Repositories/IProfessorRepository.cs
@@ -0,0 +1,15 @@
+﻿using Fiap.Exemplo02.MVC.Web.Models;
+using Fiap.Exemplo02.MVC.Web.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.Exemplo02.MVC.Web.Repositories
+{
+    public interface IProfessorRepository : IGenericRepository<Professor>
+    {
+        void Promocao(double valor);
+    }
+}
